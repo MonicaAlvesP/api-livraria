00f2b13d940586359522a514c63a1fdedf2f0211
MonicaAlvesP <monica52782@gmail.com>
2025-03-12 09:32:48 -0300

    :ok_hand: usando tag html para titulos

--- app.py
+++ app.py
@@ -1,22 +1,26 @@
-from flask import Flask
-from dotenv import load_dotenv
-import os
+from flask import Flask  # Importa a classe Flask do módulo flask
+from dotenv import load_dotenv  # Importa a função load_dotenv do módulo dotenv
+import os  # Importa o módulo os para interagir com variáveis de ambiente
 
-load_dotenv()
+load_dotenv()  # Carrega variáveis de ambiente do arquivo .env
 
-app = Flask(__name__)
+app = Flask(__name__)  # Cria uma instância da aplicação Flask
 
 
-@app.route('/')
+@app.route('/')  # Define a rota para a URL raiz
 def hello_world():
-    return 'Olá, esta é uma pagina renderizada pelo Flask!'
+    # Retorna uma mensagem para a URL raiz
+    return '<h1>Pagina home</h1>'
 
 
-@app.route('/blog')
+@app.route('/blog')  # Define a rota para a URL /blog
 def blog():
-    return 'Esta é a página inicial do blog!'
+    # Retorna uma mensagem para a URL /blog
+    return '<h1>Pagina blog</h1>'
 
 
-if __name__ == '__main__':
+if __name__ == '__main__':  # Verifica se o script está sendo executado diretamente
+    # Obtém o valor da variável de ambiente DEBUG_MODE
     debug_mode = os.getenv('DEBUG_MODE')
+    # Executa a aplicação Flask com o modo de depuração definido pela variável de ambiente
     app.run(debug=debug_mode)
